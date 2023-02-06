a182139b23faa31f1c959c19c6e09c3075022d9f
Martin Skocik <mskocik@gmail.com>
2023-02-06 18:36:52 +0100

    rework hooking vite into template

--- src/Bridges/NetteDI/ViteExtension.php
+++ src/Bridges/NetteDI/ViteExtension.php
@@ -11,40 +11,52 @@ use Mskocik\Vinette\Vite;
 
 class ViteExtension extends \Nette\DI\CompilerExtension
 {
-    public function getConfigSchema(): Schema
-    {
-        return Expect::structure([
-            'manifest' => Expect::string()->required(),
-            'assetPath' => Expect::string(null)->nullable(),
-            'devServer' => Expect::string('http://localhost:5173'),
-        ]);
-    }
+	private bool $debugMode = true;
 
-    public function beforeCompile()
-    {
-        /** @var \Nette\DI\CompilerExtension */
-        $parameters = $this->compiler->getExtensions()['parameters']->getConfig();
-        $wwwDir = $parameters['wwwDir'];
-        $debugMode = $parameters['debugMode'];
-        
-        $builder = $this->getContainerBuilder();
-        $builder->addDefinition($this->prefix('assets'))
-            ->setFactory(Vite::class)
-            ->setArguments([$this->config->devServer, ltrim($this->config->manifest, '/'), $this->config->assetPath, $wwwDir, !$debugMode]);
+	public function getConfigSchema(): Schema
+	{
+		return Expect::structure([
+			'manifest' => Expect::string()->required(),
+			'assetPath' => Expect::string(null)->nullable(),
+			'devServer' => Expect::string('http://localhost:5173'),
+		]);
+	}
 
-        $builder->getDefinition('latte.templateFactory')
-            ->addSetup('$onCreate', [[0 => $this->prefix('@assets')]]);
+	public function beforeCompile()
+	{
+		/** @var \Nette\DI\CompilerExtension */
+		$parameters = $this->compiler->getExtensions()['parameters']->getConfig();
+		$wwwDir = $parameters['wwwDir'];
+		$this->debugMode = $parameters['debugMode'];
+		
+		$builder = $this->getContainerBuilder();
+		$builder->addDefinition($this->prefix('assets'))
+			->setFactory(Vite::class)
+			->setArguments([$this->config->devServer, ltrim($this->config->manifest, '/'), $this->config->assetPath, $wwwDir, !$this->debugMode]);
+
+		$templateFactory = $builder->getDefinition('latte.templateFactory');
+		$templateFactory
+			->addSetup([self::class, 'addViteToTemplate'], [$templateFactory, $this->prefix('@assets')]);
 
 		$builder->getDefinition('latte.latteFactory')
-            ->getResultDefinition()
+			->getResultDefinition()
 			->addSetup('addFilter', ['asset', [$this->prefix('@assets'), 'getAsset']]);
-    }
+	}
 
-    public function afterCompile(ClassType $class)
-    {
-        $initialize = $this->initialization;
+	public static function addViteToTemplate(\Nette\Application\UI\TemplateFactory $factory, Vite $vite): void
+	{
+		$factory->onCreate[] = function(\Nette\Application\UI\Template $template) use ($vite) {
+			$template->vite = $vite;
+		};
+	}
+
+	public function afterCompile(ClassType $class)
+	{
+		if (!$this->debugMode) return;
+		
+		$initialize = $this->initialization;
 		$initialize->addBody('if (!Tracy\Debugger::isEnabled()) { return; }');
-        $ctor = VitePanel::class . '(\'' . $this->config->devServer . '\')';
-        $initialize->addBody("Tracy\Debugger::getBar()->addPanel(new $ctor);");
-    }
+		$ctor = VitePanel::class . '(\'' . $this->config->devServer . '\')';
+		$initialize->addBody("Tracy\Debugger::getBar()->addPanel(new $ctor);");
+	}
 }
